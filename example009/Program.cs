9566081e1ddd6a592ff1cde94763a19ce7db1f59
ruslan <rus.pro94@bk.ru>
2022-05-03 13:33:58 +0500

    E009

---
+++ example009/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {1,24,34,56,65,74,83,91,83};
+
+int n = array.Length;
+int find = 83;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index = 1;
+    index++; 
+}
+
